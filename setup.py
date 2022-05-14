e2e8b298337ae3a06e278795a3f70cfeb8c32847
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-14 19:42:25 +0900

    Update setup.py, requirements.txt

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
+import codecs
+import os
+
 from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
 
 setup(
     name='pyqt-checkbox-file-list-widget',
@@ -9,6 +18,8 @@ setup(
     packages=find_packages(),
     description='PyQt QListWidget for files and supporting the checkbox',
     url='https://github.com/yjg30737/pyqt-checkbox-file-list-widget.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.8',
         'pyqt-checkbox-list-widget>=0.0.1',
